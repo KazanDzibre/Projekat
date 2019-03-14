74b1b8ca8d08119c161ba4d51e2e79cce2ecb16b
Marko Ruzic <markoruzic997@gmail.com>
2019-03-14 21:32:24 +0100

    simple input csv

--- Projekat/Program.cs
+++ Projekat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CsvHelper;
 
 namespace Projekat
 {
@@ -6,9 +7,14 @@ namespace Projekat
     {
         static void Main(string[] args)
         {
-            class1 c1 = new class1();
+            TextReader reader = new StreamReader("import.txt");
+            var csvReader = new CsvReader(reader);
+            var records = csvReader.GetRecords<config>();
+
+            /*class1 c1 = new class1();
             Console.WriteLine(c1.ReturnMessage());
             Console.WriteLine("Hello World!");
+        */
         }
     }
 }
---
+++ Projekat/config.cs
@@ -0,0 +1,12 @@
+using System;
+using CSVHelper;
+
+namespace Projekat
+{
+    public class config
+    {
+        public string ime { get; set; }
+        public int adresa { get; set; }
+    }
+
+}
