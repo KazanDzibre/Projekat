19eb0cef7bca1122f46ccf24fc9613431e86c1a1
unknown <markoruzic997@gmail.com>
2019-03-15 21:04:55 +0100

    Cita fajlove pomocu csvReader

---
+++ CsvInput/Program.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class config
+        {
+            [Index(0)]
+            public string Name { get; set; }
+            [Index(1)]
+            public int Adress { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("C:\\Users\\Marko\\Desktop\\VS2017Project\\ConsoleApp1\\input.txt")) //ovu putanju ce sredimo
+            using (var csv = new CsvReader(reader))
+            {
+                //csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();  //Ovo za slucaj da ne odgovara header opisu
+                var records = csv.GetRecords<config>();
+            }
+        }
+    }
+}
