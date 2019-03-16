dd38b4931cca6705c9f674c41827cd56575d8b43
unknown <markoruzic997@gmail.com>
2019-03-16 17:00:39 +0100

    Napravljena mala simulacija sistema, treba jos da se doda ispis nekih mini fajlova

--- CsvInput/ConsoleApp1/Program.cs
+++ CsvInput/ConsoleApp1/Program.cs
@@ -5,12 +5,25 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        public static void interruptHandler() {
+            Console.Write("Usao u interrupt\n");
+        }
+
+        public static void threadFun() {
+            while (true) {
+                var ch = Console.Read();
+                if (ch == '1') {
+                    interruptHandler();
+                }
+            }
+        }
         public class config
         {
             [Index(0)]
@@ -25,6 +38,17 @@ namespace ConsoleApp1
             {
                 //csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();  //Ovo za slucaj da ne odgovara header opisu
                 var records = csv.GetRecords<config>();
+            }
+
+            Thread myThread = new Thread(new ThreadStart(threadFun));
+
+            myThread.Start();
+
+            //Console.Write("Hello World");
+
+            while (true) {
+                Thread.Sleep(5000);
+                Console.Write("Ovde ce biti ispis\n");
             }
         }
     }
