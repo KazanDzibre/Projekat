c71c0e4e32cc081dfd48432145ab2b274ef420e4
unknown <markoruzic997@gmail.com>
2019-04-11 16:35:03 +0200

    Promenjen ispis

--- CsvInput/ConsoleApp1/ThreadFunctions.cs
+++ CsvInput/ConsoleApp1/ThreadFunctions.cs
@@ -20,26 +20,26 @@ namespace ServerApp
         {
             Switch_off.State = "OFF";
             Switch_on.State = "ON";
-            Switch_on.Time_start = DateTime.Now.ToString("H:mm:ss");
-            Switch_off.Time_start = DateTime.Now.ToString("H:mm:ss");
+            Switch_on.Time_start = DateTime.Now.ToString("H:mm:ss:fff");
+            Switch_off.Time_start = DateTime.Now.ToString("H:mm:ss:fff");
 
             while (true)
             {
                 if (AdamComponent.switchRead() == 1 && trenutna == true)
                 {
                     Console.WriteLine("Usao na promenu ON");
-                    Switch_on.Time_start = DateTime.Now.ToString("H:mm:ss");
-                    Switch_off.Time_end = DateTime.Now.ToString("H:mm:ss");
+                    Switch_on.Time_start = DateTime.Now.ToString("H:mm:ss:fff");
+                    Switch_off.Time_end = DateTime.Now.ToString("H:mm:ss:fff");
                     FileIO.outputSwitch(switchOutList, Switch_off);
                     trenutna = false;
                 }
                 else if (AdamComponent.switchRead() == 0 && trenutna == false)
                 {
 
-                    Switch_on.Time_end = DateTime.Now.ToString("H:mm:ss");
-                    Switch_off.Time_start = DateTime.Now.ToString("H:mm:ss");
+                    Switch_on.Time_end = DateTime.Now.ToString("H:mm:ss:fff");
+                    Switch_off.Time_start = DateTime.Now.ToString("H:mm:ss:fff");
                     FileIO.outputSwitch(switchOutList, Switch_on);
-                    Console.WriteLine("Usao na promeni OFF");
+                    Console.WriteLine("Usao na promenu OFF");
                     trenutna = true;
                 }
                 AdamComponent.counterRead();
